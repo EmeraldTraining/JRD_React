0b01a2b9b6d8166fd679c17761f3611c5f7f3eb9
Lidzfreak3000 <j.r.nerdy@htomail.com>
2022-06-17 13:39:25 -0400

    chellenge 2 update. Added state and it's update.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [name, setName] = React.useState("World");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello, {name}!</h1>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
       </header>
     </div>
   );
